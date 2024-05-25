ee2aa9ce9087409237490332e09e68fb5e22b479
Vradash <singhvikramrajan111@gmail.com>
2024-05-25 15:12:55 +0530

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Card from './Components/card';
+
+function App() {
+  return (
+    <div className="container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/card.js
@@ -0,0 +1,11 @@
+
+export default function Card() {
+    return (
+        <div className="card">
+        <h3>title</h3>
+        <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <p>Card Description</p>
+        <h2>Card Title</h2>
+        </div>
+    );
+}
