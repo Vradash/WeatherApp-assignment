39b016ff73e9ab7e15956a397426b1185a172cbf
Vradash <singhvikramrajan111@gmail.com>
2024-05-26 15:55:11 +0530

    Update

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,4 +1,4 @@
-
+// import dss from '../images/thermometer.jpg'
 export default function Card(props) {
 
     let sum=0;
@@ -12,7 +12,7 @@ export default function Card(props) {
     return (
         <div className="card">
             <h3>{props?.title}</h3>
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
+            <img src={require('../images/thermometer.jpg')} alt="placeholder" />
             <div>
                 {/* <p>{props.array}</p> */}
                 <h3>{avg.toFixed(2)}</h3>
