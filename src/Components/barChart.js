356e2208109dd65fbc73275dcc5d8aa9fbe88b05
Vradash <singhvikramrajan111@gmail.com>
2024-05-30 13:09:27 +0530

    Refactor: Update BarChart component to use dynamic data from props

--- src/Components/barChart.js
+++ src/Components/barChart.js
@@ -1,30 +1,46 @@
 import CanvasJSReact from '@canvasjs/react-charts';
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+export default function BarChart(props) {
 
-const options = {
-  title: {
-    text: "Basic Column Chart in React"
-  },
-  data: [{
-    type: "column",
-    dataPoints: [
-      { label: "Apple", y: 10 },
-      { label: "Orange", y: 15 },
-      { label: "Banana", y: 25 },
-      { label: "Mango", y: 30 },
-      { label: "Grape", y: 28 }
-    ]
-  }]
-}
+    const dataArr = props.chartData?.map((element, index) => {
+        return { label: index, y: element }
+    });
 
-export default function BarChart() {
-  return (
-    <div className="container chart">
-      <CanvasJSChart options={options}
-      /* onRef = {ref => this.chart = ref} */
-      />
-    </div>
-  );
+    // var CanvasJS = CanvasJSReact.CanvasJS;
+    var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+    const options = {
+        animationEnabled: true,
+        // exportEnabled: true,
+        theme: "light1",
+        title: {text: "Weekly Temperature Chart"},
+        dataPointWidth: 80,
+        data: [{
+            type: "column",
+            // dataPoints: [
+            //     { label: "Apple", y: 10 },
+            //     { label: "Orange", y: 15 },
+            //     { label: "Banana", y: 25 },
+            //     { label: "Mango", y: 30 },
+            //     { label: "Mango", y: 30 },
+            //     { label: "Grape", y: 28 },
+            //     { label: "Grape", y: 28 },
+            // ]
+            dataPoints: dataArr || [{ label: "No Data", y: 0}]
+        }]
+    }
+
+    // console.log(props.chartData)
+    console.log(dataArr);
+
+    return (
+        <div className="container">
+            <div className="chart">
+                <CanvasJSChart options={options}
+                //  onRef = {ref => this.chart = ref} 
+                />
+            </div>
+
+        </div>
+    );
 }
