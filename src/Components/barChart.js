9442ed3b66108d0aeec32ce7e9e6db5249175208
Vradash <singhvikramrajan111@gmail.com>
2024-06-01 11:32:19 +0530

    chore: Refactor BarChart component and remove commented code

--- src/Components/barChart.js
+++ src/Components/barChart.js
@@ -17,28 +17,14 @@ export default function BarChart(props) {
         // dataPointWidth: 80,
         data: [{
             type: "column",
-            // dataPoints: [
-            //     { label: "Apple", y: 10 },
-            //     { label: "Orange", y: 15 },
-            //     { label: "Banana", y: 25 },
-            //     { label: "Mango", y: 30 },
-            //     { label: "Mango", y: 30 },
-            //     { label: "Grape", y: 28 },
-            //     { label: "Grape", y: 28 },
-            // ]
             dataPoints: dataArr || [{ label: "No Data", y: 0}]
         }]
     }
 
-    // console.log(props.chartData)
-    console.log(dataArr);
-
     return (
         // <div className="container">
             <div className="chart">
-                <CanvasJSChart options={options}
-                //  onRef = {ref => this.chart = ref} 
-                />
+                <CanvasJSChart options={options} />
             </div>
 
         // /* </div> */
